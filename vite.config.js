77a09cb9acc4bfe942b42d27dd3a57416a239072
komalparashar953 <118298862+komalparashar953@users.noreply.github.com>
2026-05-03 16:53:39 +0530

    Enhance Vite config with server and preview settings
    Added server and preview configuration options.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'team-task-manager-frontend-production-3ce2.up.railway.app',
+      'localhost'
+    ]
+  },
+  server: {
+    host: true,
+    port: 5173
+  }
 })
